42a0c946baa97b9dc49b49dd5a12cbe4a3454ada
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-04-24 00:15:32 +1000

    created installer script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF > ./style-to-here
+#!/bin/bash
+ln -s $PWD/style.scss .
+ln -s $PWD/style.css .
+EOF
+chmod +x ./style-to-here
+sudo cp ./style-to-here /usr/local/bin
+echo Created shell script "/usr/local/bin/style-to-here"
+echo contents: 
+echo
+cat ./style-to-here
+rm  ./style-to-here
